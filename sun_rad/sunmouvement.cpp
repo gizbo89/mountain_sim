6b95859f24e9a63668f5ff8202003cc134580795
Gizbo89 <delpozogalan@gmail.com>
2024-08-11 19:25:41 +0200

    Adding clarification to solar incidence module

--- sun_rad/sunmouvement.cpp
+++ sun_rad/sunmouvement.cpp
@@ -6,49 +6,60 @@ Eigen::Vector3d solar_incident_angle(const double gamma_deg, const double lat) {
   const double phi = 0.0;//precession
   const double theta = 22.5*2.0*pi/360.0;//nutation angle
   double var2;
-
-  const double philat = lat*2.0*pi/360.0;//latitude angle
+  //latitude angle in radians
+  const double philat = lat*2.0*pi/360.0;
+  ///
   const double cosphi = cos(phi);//cosinus precession angle
   const double sinphi = sin(phi);//sinus precession angle
   const double costheta = cos(theta);//cosinus nutation angle
   const double sintheta = sin(theta);//sinus nutation angle
+  ///
   double       psi      = 0.0;//initial rotation
+  //Nutation matrix
   Eigen::Matrix3d nut;
   nut << 1.0, 0.0, 0.0,
 		 0.0, costheta, sintheta,
 		 0.0, -sintheta, costheta;
+  //Precession matrix
   Eigen::Matrix3d prec;
     prec << cosphi, sinphi, 0.0,
     		-sinphi, cosphi, 0.0,
 			0.0, 0.0, 1.0;
+
   const double coslat = cos(philat);
   const double sinlat = sin(philat);
 
+  //Vector attached to earth system of reference
   Eigen::Vector3d unb3;
   unb3 << coslat, 0.0, sinlat;
 
+  //Vector representing the sun rays in a system reference attached to the sun
   Eigen::Vector3d urhob;
   unb3 << cos(gamma), sin(gamma), 0.0;
 
-  const double inc = 2.0*pi/360.0;
-  int i;
+
   Eigen::Matrix3d T;
     T << sinlat, 0.0, -coslat,
   		 0.0,    1.0,     0.0,
-  		-coslat, 0.0,  sinlat;
-  unb3 = T*unb3;
+  		coslat, 0.0,  sinlat;
+  Eigen::Vector3d unb4;
+  unb4 = T*unb3;
+
+  const double inc = 2.0*pi/360.0;
+  int i;
   for (i=0; psi<= 2*pi; i++)
 	{
 	   const double cospsi = cos(psi);
 	   const double sinpsi = sin(psi);
 	   Eigen::Matrix3d rot;
 	   rot << cospsi, sinpsi, 0.0,
-		  	  sinpsi, cospsi, 0.0,
-			  0.0,    0.0, 1.0;
+		  	  -sinpsi, cospsi, 0.0,
+			   0.0,    0.0, 1.0;
 	   Eigen::Vector3d urhob3;
 	   urhob3 = (rot*(nut*prec))*urhob;
-	   urhob3 = T*urhob3;
-	   if (urhob3.dot(unb3)>=0.0){
+	   Eigen::Vector3d urhob4;
+	   urhob4 = T*urhob3;
+	   if (urhob4.dot(unb4)>=0.0){
 
 	   }
 	   psi+=inc;
